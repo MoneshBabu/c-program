3ee0039a70e1a28ea8f33b039551ea8fb61dafdd
Monesh Babu <112542451+MoneshBabu@users.noreply.github.com>
2022-09-21 14:44:25 +0530

    Update program calculate.c

--- program calculate.c
+++ program calculate.c
@@ -1,14 +1,44 @@
 #include<stdio.h>
-int main()
+#include<math.h>
+void power(int a,int b)
 {
-	int sub(int,int);
-	int x,n;
-	printf("enter values");
-	scanf("%d%d",&x,&n);
-	printf("siub = %d",add(x,n));
-	return 0;
+	int m;
+	m = pow(a,b);
+	printf("%lli\n",m);	
 }
-int add(int x, int n)
+void add(int a,int b)
 {
-	return(x-n);
+    int m;
+	m = a + b;
+	printf("%d\n",m);		
+}
+void sub(int a,int b)
+{
+	int m;
+	m = a - b;
+	printf("%d\n",m);		
+}
+void mul(int a,int b)
+{
+	int m,n;
+	m = a * b;
+	printf("%d",m);		
+}
+void div(int a,int b)
+{
+	float m;
+	m = a/b;
+	printf("%f",m);	
+}
+
+void main()
+{
+	int a,b;
+	printf("Enter two numbers: ");
+	scanf("%d %d",&a,&b);
+	power(a,b);
+	add(a,b);
+	sub(a,b);
+	mul(a,b);
+	div(a,b);
 }
